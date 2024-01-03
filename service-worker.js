c407fffeb73e8c9cca730c2758652a6d31460e7f
mturel <155528571+mturel@users.noreply.github.com>
2024-01-03 10:56:11 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'renpy-web-game';
+var cacheName = 'marg-01';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
